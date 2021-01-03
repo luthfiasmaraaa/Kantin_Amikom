da96dd1843ace9d31aad24e152407fd403c0b40b
luthfiasmaraaa <76767509+luthfiasmaraaa@users.noreply.github.com>
2021-01-04 00:22:16 +0700

    Add files via upload

---
+++ Daftar.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kantin_Amikom
+{
+	/// <summary>
+	/// Interaction logic for Daftar.xaml
+	/// </summary>
+	public partial class Daftar : Window
+	{
+		public Daftar()
+		{
+			InitializeComponent();
+		}
+		public void Button_Click(object sender, RoutedEventArgs e)
+		{
+			SqlConnection con = new SqlConnection("Data Source=DESKTOP-TB88EJL;Initial Catalog=Kantin_Amikom;Integrated Security=True");
+			SqlCommand cmd = new SqlCommand("Insert into tblogin values(@Username, @Password)", con);
+			cmd.Parameters.AddWithValue("@Username", uname.Text);
+			cmd.Parameters.AddWithValue("@Password", pass.Text);
+			con.Open();
+			cmd.ExecuteNonQuery();
+			MessageBox.Show("Akun berhasil dibuat"); 
+			con.Close();
+			
+		}
+
+		public void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			MainWindow kembali = new MainWindow();
+			kembali.Show();
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kantin_Amikom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-TB88EJL;Initial Catalog=Kantin_Amikom;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("select count (*) from tblogin where Username= '" + username.Text + "' and Password ='" + password.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Menu menu = new Menu();
+                menu.Show();
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password Salah", "Perhatian!", MessageBoxButton.OK);
+            }
+
+        }
+
+        public void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Daftar daftar = new Daftar();
+            daftar.Show();
+            
+        }
+    }
+}
